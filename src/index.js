3ef3e67f51e462b225801ed33c0edc9845e20d5d
Evtihevich Masha <evtihevich1996@gmail.com>
2021-06-14 18:50:48 +0300

    feat: change the value

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import Square from './Props-2';
 ReactDOM.render(
   <React.StrictMode>
     <div className='text__container'>
-      <Text text='Regular text' fontSize={20} colorText='green' underline={true} />
+      <Text text='Regular text' fontSize={20} colorText='green' underline={false} />
       <Text text='Huge header' fontSize={50} colorText='black' underline={false} />
-      <Text text='Danger notification' fontSize={27} colorText='red' underline={true} />
-      <Text text='Underlined text' fontSize={40} colorText='yellow' underline={false} />
+      <Text text='Danger notification' fontSize={27} colorText='red' underline={false} />
+      <Text text='Underlined text' fontSize={40} colorText='yellow' underline />
     </div>
     <div className='square__container'>
       <Square className='square-big' label='Big square' />
